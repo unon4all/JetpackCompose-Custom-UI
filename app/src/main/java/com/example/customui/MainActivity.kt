b7276bba3d6d6a1169244e572f6bd85c16e62f64
unon <unon4all@gmail.com>
2024-05-24 01:14:51 +0530

    0.0.2 - Ball Tracker Game

--- app/src/main/java/com/example/customui/MainActivity.kt
+++ app/src/main/java/com/example/customui/MainActivity.kt
@@ -4,28 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import com.example.customui.components.MyCanvas
-import com.example.customui.ui.theme.CustomUITheme
+import com.example.customui.components.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CustomUITheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(), containerColor = Color.White
-                ) { innerPadding ->
-                    MyCanvas(modifier = Modifier.padding(innerPadding))
-                }
-            }
+            MainScreen()
         }
     }
 }
-
-
